dba89ecdd627bc6bb765e5d275250f9bfdc11f7a
cheikhtidiane <cheikhtidiane15.thiam@ucad.edu.sn>
2024-09-13 10:39:51 +0000

    second commit

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent {
+
+
+  title : string= "cheikh";
+  description : string ="cheikh est un developpeur front backend passionné d'Angular  ";
+  image : string ='ppyang.jpg';
+
+
+
+
+}
